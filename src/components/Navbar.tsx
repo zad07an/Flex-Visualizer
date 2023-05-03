488e24c4d71f639949c63a9ce6f093f99d509988
zad07an <aramzadoyan5@gmail.com>
2023-05-03 16:12:22 +0400

    Flex visualizer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar: React.FC = () => {
   return (
     <header className=" w-full h-[60px] px-[60px] flex items-center justify-center bg-blue-600">
-      <p className=" text-white uppercase text-2xl font-bold">Flex visualizer</p>
+      <p className=" text-white uppercase text-2xl font-bold">Flex visualizer <span className=" text-sm font-thin">by Aram Zadoyan</span></p>
     </header>
   );
 };
